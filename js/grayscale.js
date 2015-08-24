da79e7d2c8e184b2839921180ba9b2890376f0d8
norjen <jnorgaard@gmail.com>
2015-08-24 15:05:37 +0200

    First real stuff

--- js/grayscale.js
+++ js/grayscale.js
@@ -41,7 +41,8 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        //center: new google.maps.LatLng(40.6700, -73.9400), // New York
+		center: new google.maps.LatLng(55.652076, 12.302310), // Taastrup
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
